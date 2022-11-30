a129d13c139b652ac3201eff13efbfc0b1450c85
paivalucass <lucaspaiva2805@gmail.com>
2022-11-30 13:20:54 -0300

    feature/AdventagesSectionCRUD: added the Adventages and Hiring section CRUD

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import AdventageSectionController from '@controllers/AdventageSectionController';
+import HiringSectionController from '@controllers/HiringSectionController';
 
 const routes = express.Router();
 const userController = new UserController();
+const adventageController = new AdventageSectionController();
+const hiringController = new HiringSectionController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
+routes.post('/adventage', adventageController.create);
+routes.get('/adventage', adventageController.get);
+routes.delete('/adventage/:id', adventageController.delete);
+routes.put('/adventage/:id', adventageController.update);
+
+routes.post('/hiring', hiringController.create);
+routes.get('/hiring', hiringController.get);
+routes.delete('/hiring/:id', hiringController.delete);
+routes.put('/hiring/:id', hiringController.update);
 
 export default routes;
