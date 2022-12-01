625cbc3fef9a60176d8469924d6249bd342f1674
caiovmfc <caio.fern58@gmail.com>
2022-12-01 01:12:09 -0300

    navbar: progresso inicial

---
+++ client/src/pages/navbar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavbarContainer } from "./styles";
+//baixar e importar imagens da logo
+
+export const Navbar: React.FC = () => {
+    return (
+        <NavbarContainer>
+            <div className="group43"></div>
+        </NavbarContainer>
+    );
+}
---
+++ client/src/pages/navbar/styles.ts
@@ -0,0 +1,200 @@
+import styled from 'styled-components'; 
+
+//implementar animação cores, redirecionamento e barra fixa.
+export const NavbarContainer = styled.div`
+position: absolute;
+width: 1366px;
+height: 110px;
+left: 0px;
+top: -110px;
+filter: drop-shadow(0px -6px 25px rgba(0, 0, 0, 0.24));
+
+.group43 {
+    position: absolute;
+    width: 107px;
+    height: 84px;
+    left: 115px;
+    top: 16px;    
+}
+
+.Tmat {
+    position: absolute;
+    width: 104px;
+    height: 55px;
+    left: 116px;
+    top: 40px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 45.9911px;
+    line-height: 55px;
+    text-align: center;
+    letter-spacing: 0.09em;
+    
+    background: linear-gradient(182.67deg, rgba(122, 255, 247, 0.77) 12.71%, rgba(255, 255, 255, 0) 67.02%), #59B8FD;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+    
+    text-shadow: 0px 1.91049px 1.91049px rgba(0, 0, 0, 0.25);
+}
+
+.group41 {
+    position: absolute;
+    width: 107px;
+    height: 84px;
+    left: 115px;
+    top: 16px;
+}
+
+.Tsust {
+    position: absolute;
+    width: 107px;
+    height: 84px;
+    left: 115px;
+    top: 16px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 34.6402px;
+    line-height: 42px;
+    text-align: center;
+    letter-spacing: 0.115em;
+    
+    background: linear-gradient(176.5deg, #636363 8.17%, #040404 86.35%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+}
+
+.Tteam {
+    position: absolute;
+    width: 58px;
+    height: 25px;
+    left: 1102px;
+    top: 43px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    
+    color: #222222;
+}
+
+.Ttechnology {
+    position: absolute;
+    width: 113px;
+    height: 25px;
+    left: 935px;
+    top: 43px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    text-align: center;
+    
+    color: #222222;
+}
+
+.Tadvantages {
+    position: absolute;
+    width: 113px;
+    height: 25px;
+    left: 768px;
+    top: 43px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    text-align: center;
+    
+    color: #222222;
+}
+
+.Thome {
+    position: absolute;
+    width: 62px;
+    height: 25px;
+    left: 652px;
+    top: 43px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 24px;
+    text-align: center;
+    
+    background: linear-gradient(280.74deg, #52A5E1 54.39%, #7EDAF6 138.84%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
+}
+
+.contact {
+    position: absolute;
+    width: 138px;
+    height: 42px;
+    left: 1194px;
+    top: 34px;
+}
+
+.Tcontact {
+    position: absolute;
+    width: 78px;
+    height: 25px;
+    left: 1224px;
+    top: 43px;
+    
+    font-family: 'Sen';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    
+    color: #FFFFFF;
+    
+    text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.17);
+}
+
+.group45 {
+    position: absolute;
+    width: 138px;
+    height: 42px;
+    left: 1194px;
+    top: 34px;
+}
+
+.rectangle150 {
+    position: absolute;
+    width: 138px;
+    height: 42px;
+    left: 1194px;
+    top: 34px;
+    
+    background: linear-gradient(278.87deg, #6AA5FE 27.2%, #73D5E5 111.13%);
+    border-radius: 21px;
+}
+
+.rectangle2 {
+    position: absolute;
+    width: 1366px;
+    height: 110px;
+    left: 0px;
+    top: 0px;
+    
+    background: #FFFFFF;
+    box-shadow: 0px 2px 30px rgba(0, 0, 0, 0.1);
+}
+`;
