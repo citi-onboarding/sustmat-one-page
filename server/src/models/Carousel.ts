3ad1fe2b83ca9860837f40f6f727907bca7b1cf2
caiovmfc <caio.fern58@gmail.com>
2022-12-04 02:38:45 -0300

    CRUD Carousel: primeiras alterações

--- server/src/models/Carousel.ts
+++ server/src/models/Carousel.ts
@@ -1,18 +1,11 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
-export class User {
+export class Carousel {
 
     @PrimaryGeneratedColumn()
     id: number
 
     @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
+    link: string
 }
