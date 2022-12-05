e88678d8459e7f809543e6d0e4c934f1e9300755
caiovmfc <caio.fern58@gmail.com>
2022-12-05 00:52:22 -0300

    footer: ajustes iniciais

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,20 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
 
-
 import { Home } from './pages';
+import { Footer } from './pages';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home/>
-      <GlobalStyle/>
-    </ThemeProvider>
+    <Footer/>
   );
 }
 
 export default App;
+
+/*
+    <ThemeProvider theme={theme}>
+      <Home/>
+      <GlobalStyle/>
+    </ThemeProvider>
+*/
