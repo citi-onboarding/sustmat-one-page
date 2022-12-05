daad6383a4d2882f336b75321a4b0f85a7bea603
caiovmfc <caio.fern58@gmail.com>
2022-12-05 13:43:09 -0300

    footer: primeiros passos

--- client/src/pages/footer/index.tsx
+++ client/src/pages/footer/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { FooterContainer } from "./styles";
+import { LogoSustMat } from "../../assets";
 
 export const Footer: React.FC = () => {
     return(
         <FooterContainer>
+            <div className="group43">
+                <img src={LogoSustMat} alt="" />
+            </div>
         </FooterContainer>
     );
 }
--- client/src/pages/footer/styles.ts
+++ client/src/pages/footer/styles.ts
@@ -1,4 +1,19 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
+position: absolute;
+width: 1366px;
+height: 415px;
+left: 0%;
+margin: auto;
+top: 3162px;
+background-color: black;
+
+.group43 {
+    position: absolute;
+    left: 8.78%;
+    right: 81.11%;
+    top: 41.2%;
+    bottom: 32.77%;
+}
 `
