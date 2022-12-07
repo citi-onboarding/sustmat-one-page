342e7d98724f49ecc1ec77002b69d370259386bb
paivalucass <lucaspaiva2805@gmail.com>
2022-12-07 13:39:14 -0300

    Added background image

--- client/src/pages/Advantages/styles.ts
+++ client/src/pages/Advantages/styles.ts
@@ -7,7 +7,7 @@ export const AdvantagesSectionContainer = styled.div`
     justify-content: center;
     width: 1440px;
     height: 657px;
-    background-color: grey;
+    background-color: white;
 
 `;
 
@@ -20,7 +20,8 @@ export const TitleSection = styled.h1`
     line-height: 49px;
     text-align: right;
     color: #373737;
-    margin: 116px 115px 445px 685px;
+    margin: 116px 97px 445px 658px;
+    
     #SustmatWord{
         background: linear-gradient(288.52deg, #5191F1 40.92%, #7DDCF7 101.83%);
         -webkit-background-clip: text;
@@ -43,4 +44,14 @@ export const AdvantagesCardContainer = styled.div`
         padding: 65px;
     }
 `
+
+export const ImageContainer = styled.div`
+    width: 887px;
+    height: 629px;
+    position: absolute;
+
+    #BackgroundImage{
+        position: absolute;
+    }
+`;
 
