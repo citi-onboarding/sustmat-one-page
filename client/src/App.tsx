9831e08b2c3b1ae9186e9b036f04de09f8293f27
Marcos Antonio <55698235+marcoslima12@users.noreply.github.com>
2022-12-08 11:52:35 -0300

    Revert "Feature/footer"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,34 +4,19 @@ import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
 
 import { Home } from './pages';
-<<<<<<< HEAD
-import { Footer } from './pages';
-
-function App() {
-  return (
-    <Footer/>
-=======
 import { Navbar } from './pages';
 
 function App() {
   return (
       <Navbar/>
->>>>>>> develop
   );
 }
 
 export default App;
 
 /*
-<<<<<<< HEAD
-    <ThemeProvider theme={theme}>
-      <Home/>
-      <GlobalStyle/>
-    </ThemeProvider>
-=======
 <ThemeProvider theme={theme}>
 <Home/>
 <GlobalStyle/>
 </ThemeProvider>
->>>>>>> develop
 */
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,25 +1,7 @@
 import LogoCITi from './logo.png';
-<<<<<<< HEAD
-import LogoSustMat from './Group 43.png'
-import Face from './facebook.png'
-import Insta from './instagram.png'
-import YouTb from './youtube.png'
-import CitiBranco from './CITi.png'
-import Heart from './Vector.png'
-
-export {
-    LogoCITi,
-    LogoSustMat,
-    YouTb,
-    Face,
-    Insta,
-    CitiBranco,
-    Heart
-=======
 import LogoSustMat from './Group 43.png';
 
 export {
     LogoCITi,
     LogoSustMat
->>>>>>> develop
 };
