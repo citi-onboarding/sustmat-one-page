4c805c7e064458734110cccfaa3b91ebf96fa14a
paivalucass <lucaspaiva2805@gmail.com>
2022-12-08 11:38:08 -0300

    Solved merge conflicts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,15 +1,9 @@
 import { Home } from './home';
-<<<<<<< HEAD
 import { Footer } from './footer';
-
-export {
-    Home,
-    Footer
-=======
 import { Navbar } from './navbar';
 
 export {
     Home,
-    Navbar
->>>>>>> develop
+    Navbar,
+    Footer
 };
