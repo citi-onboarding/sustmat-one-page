18626dbdb9c065d4e1a77d1d62a4f740f4df6a13
caiovmfc <caio.fern58@gmail.com>
2022-12-13 20:35:09 -0300

    fix/navbar: each button redirects to its section

--- client/src/pages/navbar/index.tsx
+++ client/src/pages/navbar/index.tsx
@@ -12,15 +12,25 @@ export const Navbar: React.FC = () => {
     return (
         <NavbarContainer>
             <img src={LogoSustMat} alt="" />
+            <a href="#team">
             <button onClick={() => SetChangeColor1(!changeColor1)} className= {changeColor1? 'Tteam' : 'blueteam' }>Team</button>
+            </a>
+            <a href="#">
             <button onClick={() => SetChangeColor2(!changeColor2)} className= {changeColor2? 'Ttechnology' : 'bluetech' }>Technology</button>
+            </a>
+            <a href="#advantages">
             <button onClick={() => SetChangeColor3(!changeColor3)} className= {changeColor3? 'Tadvantages' : 'blueadva' }>Advantages</button>
+            </a>
+            <a href="#banner">
             <button onClick={() => SetChangeColor4(!changeColor4)} className= {changeColor4? 'Thome' : 'bluehome' }>Home</button>
+            </a>
             <div className="contact">
                 <div className="group45">
+                    <a href="#">
                     <button onClick={() => SetChangeColor5(!changeColor5)} className= {changeColor5? 'rectangle150' : 'bluerectangle150' }>
                         <div className="Tcontact">Contact</div>
                     </button>
+                    </a>
                 </div>
             </div>
         </NavbarContainer>
