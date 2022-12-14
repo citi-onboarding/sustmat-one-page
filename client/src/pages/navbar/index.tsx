86ecb336ee76536514c8df4f2ff66b8d22d94981
caiovmfc <caio.fern58@gmail.com>
2022-12-14 11:14:02 -0300

    fix/navbar: all adjusted

--- client/src/pages/navbar/index.tsx
+++ client/src/pages/navbar/index.tsx
@@ -13,21 +13,21 @@ export const Navbar: React.FC = () => {
         <NavbarContainer>
             <img src={LogoSustMat} alt="" />
             <a href="#team">
-            <button onClick={() => SetChangeColor1(!changeColor1)} className= {changeColor1? 'Tteam' : 'blueteam' }>Team</button>
+            <button onClick={() => {SetChangeColor1(false); SetChangeColor2(true); SetChangeColor3(true); SetChangeColor4(true); SetChangeColor5(true)}} className= {changeColor1? 'Tteam' : 'blueteam' }>Team</button>
             </a>
             <a href="#">
-            <button onClick={() => SetChangeColor2(!changeColor2)} className= {changeColor2? 'Ttechnology' : 'bluetech' }>Technology</button>
+            <button onClick={() => {SetChangeColor1(true); SetChangeColor2(false); SetChangeColor3(true); SetChangeColor4(true); SetChangeColor5(true)}} className= {changeColor2? 'Ttechnology' : 'bluetech' }>Technology</button>
             </a>
             <a href="#advantages">
-            <button onClick={() => SetChangeColor3(!changeColor3)} className= {changeColor3? 'Tadvantages' : 'blueadva' }>Advantages</button>
+            <button onClick={() => {SetChangeColor1(true); SetChangeColor2(true); SetChangeColor3(false); SetChangeColor4(true); SetChangeColor5(true)}} className= {changeColor3? 'Tadvantages' : 'blueadva' }>Advantages</button>
             </a>
             <a href="#banner">
-            <button onClick={() => SetChangeColor4(!changeColor4)} className= {changeColor4? 'Thome' : 'bluehome' }>Home</button>
+            <button onClick={() => {SetChangeColor1(true); SetChangeColor2(true); SetChangeColor3(true); SetChangeColor4(false); SetChangeColor5(true)}} className= {changeColor4? 'Thome' : 'bluehome' }>Home</button>
             </a>
             <div className="contact">
                 <div className="group45">
                     <a href="#">
-                    <button onClick={() => SetChangeColor5(!changeColor5)} className= {changeColor5? 'rectangle150' : 'bluerectangle150' }>
+                    <button onClick={() => {SetChangeColor1(true); SetChangeColor2(true); SetChangeColor3(true); SetChangeColor4(true); SetChangeColor5(false)}} className= {changeColor5? 'rectangle150' : 'bluerectangle150' }>
                         <div className="Tcontact">Contact</div>
                     </button>
                     </a>
