7002f27247ba5a3b7cb33be501e312f162166ace
Marcos Antonio <marcosantonio-10@live.com>
2022-12-19 15:06:03 -0300

    feature/finalizando

--- client/src/pages/Advantages/index.tsx
+++ client/src/pages/Advantages/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AdvantagesCardContainer, AdvantagesSectionContainer, TitleSection } from "./styles";
 import { AdvantagesCard } from "../../components";
+import api from "../../services/api";
+
+type AdavantageData = {
+    id: number;
+    title: string;
+    description: string;
+}
 
 
 export const AdvantagesSection: React.FC = () =>{
+    const [data, setData] = useState<AdavantageData[]>([]);
+
+    async function getData() {
+        try {
+            const response = await api.get(`/adventage`);
+            setData(response.data);
+            console.log("oi" + data)
+        } catch (error) {
+            console.log('Error connecting to database');
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+    
     return(
         <AdvantagesSectionContainer id="Advantages">
             <TitleSection>
@@ -11,12 +33,12 @@ export const AdvantagesSection: React.FC = () =>{
             </TitleSection>
             <AdvantagesCardContainer>
                 <AdvantagesCard icon={"https://lh3.googleusercontent.com/pw/AL9nZEUOJXigOOrRQo7ulkyVu9Ratr5F2zlSmmwRCp7-9r7ptz-KOrvxE7RKpBORfOebib3Y2NadpsxgIzdRzcdOjrMvzHuzcakUJ8zzPuFKJogMMKFW8ps4kMSdO6yegrEgQtFm5KzoScw_JpM2ZFM6J3Q=w56-h43-no?authuser=3"}
-                title={"Less damage to the nature"} description={"With our smart, innovative and disruptive technology we are able to mine gold sustainably"}/>
+                title={data[0].title} description={"With our smart, innovative and disruptive technology we are able to mine gold sustainably"}/>
                 <AdvantagesCard icon={"https://lh3.googleusercontent.com/pw/AL9nZEVyStOuKJPoNRQWT_z30tgkRMCW6E9bdLciw2vmYRx9rkzzKpoinLDlXOMyZEFio0_Tpv_COQMVG5SvEckqAmnszapMiZa25jNd5cCSGig1jypqTl70ZbuVWGcYvoidTh0KfK1j0Vf4OB5Owb-AqNI=w35-h46-no?authuser=3"}
                 title={"More revenue"} description={"We increase the revenue that can be invested in ESG actions, ecological closure of the area and in socio-environmental actions"}/>
                 <AdvantagesCard icon={"https://lh3.googleusercontent.com/pw/AL9nZEVfUKcncxXUfHJP71Z9hRsJftucFYYMGr4CcdpxMeESzrvu6Dd-qDDyJTJ9Rls_qwaeBxIg1uXDBpdH1g1jYN70wFKl4XgOUKNjiHFEr5NNWferP6ElTNSCQga9Dol94wqPiDRkPOci_I8Y_Wur-X0=w42-h35-no?authuser=3"}
                 title={"High technology"} description={"We have patents for using the effects of liquefaction to do mineral separation and dewatering"}/>
-            </AdvantagesCardContainer>
+            </AdvantagesCardContainer> 
         </AdvantagesSectionContainer>
     );
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,18 @@
 import express from 'express';
 import CarouselController from '@controllers/CarouselController';
+import AdventageSectionController from '@controllers/AdventageSectionController';
+import HiringSectionController from '@controllers/HiringSectionController';
 
 const routes = express.Router();
+
 const carouselController = new CarouselController();
+const adventageController = new AdventageSectionController();
+const hiringController = new HiringSectionController();
 
 routes.post('/carousel', carouselController.create);
 routes.get('/carousel', carouselController.get);
 routes.delete('/carousel/:id', carouselController.delete);
 routes.put('/carousel/:id', carouselController.update);
-import UserController from '@controllers/UserController'
-import AdventageSectionController from '@controllers/AdventageSectionController';
-import HiringSectionController from '@controllers/HiringSectionController';
-
-const routes = express.Router();
-const userController = new UserController();
-const adventageController = new AdventageSectionController();
-const hiringController = new HiringSectionController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/adventage', adventageController.create);
 routes.get('/adventage', adventageController.get);
