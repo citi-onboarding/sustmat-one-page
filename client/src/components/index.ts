d23dcaa8ffa5d3a5f1b8711b2e4625933e168ff9
Marcos Antonio <marcosantonio-10@live.com>
2022-12-20 10:42:32 -0300

    feature/integracoes: Made integration os 2/3

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import { CardComponent } from "./Card";
 import { AdvantagesCard } from "./CardAdvantages";
-import SimpleSlider from "./CarouselSlide";
+import {SimpleSlider} from "./CarouselSlide";
 
 export {
     CardComponent,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,18 @@
 import express from 'express';
+import AdventageSectionController from '@controllers/AdventageSectionController';
+import HiringSectionController from '@controllers/HiringSectionController';
 import CarouselController from '@controllers/CarouselController';
 
 const routes = express.Router();
+
+const adventageController = new AdventageSectionController();
+const hiringController = new HiringSectionController();
 const carouselController = new CarouselController();
 
+routes.put('/carousel/:id', carouselController.update);
 routes.post('/carousel', carouselController.create);
 routes.get('/carousel', carouselController.get);
 routes.delete('/carousel/:id', carouselController.delete);
-routes.put('/carousel/:id', carouselController.update);
-import UserController from '@controllers/UserController'
-import AdventageSectionController from '@controllers/AdventageSectionController';
-import HiringSectionController from '@controllers/HiringSectionController';
-
-const routes = express.Router();
-const userController = new UserController();
-const adventageController = new AdventageSectionController();
-const hiringController = new HiringSectionController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/adventage', adventageController.create);
 routes.get('/adventage', adventageController.get);
