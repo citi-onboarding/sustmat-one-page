eec1a75b6bdbc41c114484acc5c54acb00a6caaa
Marcos Antonio <marcosantonio-10@live.com>
2022-12-20 10:54:53 -0300

    feature/integracoes: made everything

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,12 +1,33 @@
-import ReactS, { ReactElement } from "react";
+import ReactS, { ReactElement, useEffect, useState } from "react";
 import { StyledCard } from "./styles";
 import { Certinho } from "../../assets";
+import api from "../../services/api";
 
 type CardProps = {
     technician: string;
 }
 
+type CardData = {
+    requirements: string[];
+}
+
 export const CardComponent: React.ElementType = ({technician}:CardProps) => {
+
+
+    const [data, setData] = useState<CardData[]>([]);
+
+    async function getData() {
+        try {
+            const response = await api.get(`/hiring`);
+            setData(response.data);
+        } catch (error) {
+            console.log('Error connecting to database');
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
     <StyledCard>
         <button className="knowmorebutton">
@@ -15,11 +36,12 @@ export const CardComponent: React.ElementType = ({technician}:CardProps) => {
             </div>
         </button>
         <div className="group447">
-            <h1 className="parttime">
+            {/* <h1 className="parttime">
                 Part-time
                 <br></br><br></br>
                 Make the recovery of metals from extractor solution.
-            </h1>
+            </h1> */}
+            {data.map((requirement)=> <h1 className="parttime">{requirement.requirements}</h1> )}
             <h1 className="requirements">Requirements</h1>
             <h1 className="technician">{technician}</h1>
             <img className="certinho" src={Certinho} alt="" />
--- client/src/pages/hiringsection/index.tsx
+++ client/src/pages/hiringsection/index.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Components, HiringContainer } from "./styles";
 import { CardComponent } from "../../components";
-import { Certinho } from "../../assets";
 import { BlueBar } from "../../assets";
+import api from "../../services/api";
+
+type HiringData = {
+    job: string;
+}
 
 export const HiringSection: React.FC = () => {
+
+    const [data, setData] = useState<HiringData[]>([]);
+
+    async function getData() {
+        try {
+            const response = await api.get(`/hiring`);
+            setData(response.data);
+        } catch (error) {
+            console.log('Error connecting to database');
+        }
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <HiringContainer id="Hiring">
             <img id="vector21" src={BlueBar} alt="" />
             <h1 className="wearehiring">We are hiring!</h1>
             <h1 className="beapart">Be a part of the sustmat team.</h1>
             <Components>
+                {/* <CardComponent technician={"Chemistry Technician"}/>
                 <CardComponent technician={"Chemistry Technician"}/>
-                <CardComponent technician={"Chemistry Technician"}/>
-                <CardComponent technician={"Processing Technician"}/>
+                <CardComponent technician={"Processing Technician"}/> */}
+                {data.map((card)=><CardComponent technician={card.job}/>)}
             </Components>
         </HiringContainer>
     );
